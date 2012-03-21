fbf80f50956174a3a4d1ee44172048876c489982
tak.hasegawa <tak.es.hasegawa+github@gmail.com>
2012-03-21 23:10:47 +0900

    第2巻 13章まで

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,41 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end 
   
+  def new
+    @category = Category.new
+  end
+  
+  def show
+    redirect_to :categories, :status => 301
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to :categories
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    @category.attributes = params[:category]
+    if @category.save
+      redirect_to :categories
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to :categories
+  end
+  
 end
