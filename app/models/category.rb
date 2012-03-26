7c295a5be880c334ca6e1cbbd2efeece988bafcc
tak.hasegawa <tak.es.hasegawa+github@gmail.com>
2012-03-26 18:46:25 +0900

    第2巻 15章まで

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,20 @@
+require 'nkf'
+
 class Category < ActiveRecord::Base
   has_many :tasks, :dependent => :nullify
   
+  before_validation :normalize_values
+  
   validates :name, :presence => true, :length => {:maximum => 20}
+  validates :name, :uniqueness => { :case_sensitive => false }
+
+  ######################
+  private
+  ######################
+  def normalize_values
+     if name.present?
+       self.name = NKF.nkf("-WwZ", name)
+       self.name.strip!
+     end
+  end
 end
