3c8b4c17a98abbcfe84bb7f93207b163336d71fd
tak.hasegawa <tak.es.hasegawa+github@gmail.com>
2012-03-29 22:22:56 +0900

    第2巻 16章まで

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,3 +58,9 @@ Hinagiku::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+class ActionDispatch::Request
+  def local?
+    false
+  end
+end
